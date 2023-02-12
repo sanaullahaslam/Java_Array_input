3332b95ee221ce174e55e18ab06c741727a17171
Sanaullah Aslam <107422943+sanaullahaslam@users.noreply.github.com>
2023-02-12 11:13:05 -0800

    Add files via upload

---
+++ Array_input.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class Array_input {
+
+public static void main (String args []){
+
+Scanner ab=new Scanner(System.in);
+
+String array[]={"Sanaullah ","Sheeraz ","Naveed ","Najeeb ","Hamid ","Sir Fahad "};
+int num[]={1,2,3,4,5,6};
+for(int i=0;i<6;i++){	
+System.out.println(array[i]);
+for(int a=0;a<6;a++){	
+}
+System.out.println(" Input your index");
+int ac=ab.nextInt();
+System.out.println(num[ac] +","+array[ac]);
+
+
+	}	
+	}	
+	}
